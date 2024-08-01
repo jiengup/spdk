78f2c193ef9d74992f49804c3358536509e1a2c6
GuntherXing <guntherxing@tencent.com>
2024-08-01 17:31:09 +0800

    gc algorithm

--- exp/job/gen_job.py
+++ exp/job/gen_job.py
@@ -2,13 +2,14 @@ import configparser
 import itertools
 import os
 
-FIO_JOB_FILE_BASE = "{}_{}_op{}.job"
+FIO_JOB_FILE_BASE = "{}_{}_op{}_{}.job"
 SPDK_CONFIG_JSON_BASE = "/data/guntherxing/dev/spdk/exp/config/{}_ftl_op{}.json"
 
 BS = ["4k", "16k", "32k", "64k", "128k"]
 WM = ["rand", "seq"]
 OP = ["20", "40", "60"]
-ALGO = ["random_group", "single_group"]
+ALGO = ["random_group", "single_group", "sepbit", "sepgc", "mida"]
+DIST = ["uni", "zipf:0.8", "zipf:1.2"]
 
 def erase_space(filep):
     with open(filep, 'r') as file:
@@ -19,11 +20,11 @@ def erase_space(filep):
     with open(filep, 'w') as file:
         file.writelines(processed_lines)
 
-for algo, bs, wm, op in itertools.product(ALGO, BS, WM, OP):
+for dist, algo, bs, wm, op in itertools.product(DIST, ALGO, BS, WM, OP):
     config = configparser.ConfigParser()
     config.read('template.job')
 
-    job_file = FIO_JOB_FILE_BASE.format(bs, wm, op)
+    job_file = FIO_JOB_FILE_BASE.format(bs, wm, op, dist)
     spdk_json_conf = SPDK_CONFIG_JSON_BASE.format(algo, op)
     config.set('global', 'spdk_json_conf', spdk_json_conf)
     if wm == "rand":
@@ -32,6 +33,10 @@ for algo, bs, wm, op in itertools.product(ALGO, BS, WM, OP):
         config.set('job1', 'rw', "write")
     else:
         raise RuntimeError
+    
+    if dist != "uni":
+        config.set('global', "random_distribution", dist)
+    
     config.set('global', 'bs', bs)
 
     if not os.path.exists(algo):
